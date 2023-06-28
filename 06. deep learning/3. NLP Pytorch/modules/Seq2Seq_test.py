cd2cf0eed8fbe97dca2d052c71e27a7ead8cf5cc
parking <hyenzzang94@gmail.com>
2023-06-28 15:06:59 +0000

    23.06.29

--- 06. deep learning/3. NLP Pytorch/modules/Seq2Seq_test.py
+++ 06. deep learning/3. NLP Pytorch/modules/Seq2Seq_test.py
@@ -39,8 +39,11 @@ class ConverseWordDataset(Dataset):
     ######################
     # 데이터셋 필수 함수 정의
     ######################
-    def __init__(self, X, y=None, max_len=10, vocab=None):
+    def __init__(self, X, y=None, max_len=10, vocab=None, embedding_type='emb'):
         super().__init__()
+        # 모드 저장
+        self.is_train_mode = True if y is not None else False
+        self.embedding_type = embedding_type
         # 최대 길이, vocab 저장
         self.max_len, self.vocab = max_len, vocab
         # one hot encoding vector
@@ -53,18 +56,28 @@ class ConverseWordDataset(Dataset):
             # 디코더의 입력값과 출력값을 구분하기 위해 시작 토큰과 끝 토큰을 추가
             self.y_dec = self.__add_se_token(_y)
             self.y_target = self.__add_se_token(_y, sep_tok='E')
+            self.y_fake = self.__get_eval_dec_input()
         else:
             # y가 없으면 디코더의 기본 입력값만 생성
-            self.y_dec = self.__get_eval_dec_input()
-            self.y_target = None
+            self.y_dec = None
+            self.y_target = self.__get_eval_dec_input()
+            self.y_fake = self.__get_eval_dec_input()
     
     def __len__(self):
         return len(self.X)
 
     def __getitem__(self, idx):
+        # 임베딩 타입이 emb일 경우
+        if self.embedding_type == 'emb':
+            return self.__getitem_emb(idx)
+        elif self.embedding_type == 'ohe':
+            return self.__getitem_ohe(idx)
+    
+    def __getitem_emb(self, idx):
         # 인코더의 입력값을 tensor로 변환
         feature = torch.tensor(self.X[idx], dtype=torch.int32)
-        if self.y_target is not None:
+        # y값이 있고 학습 모드일 때
+        if self.y_target is not None and self.is_train_mode:
             # 디코더의 입력값(SE토근이 추가된 y)을 tensor로 변환
             dec_input = torch.tensor(self.y_dec[idx], dtype=torch.int32)
             # 타겟값을 ohe 후 tensor로 변환 -> pred값과 비교하기 위해 ohe
@@ -73,14 +86,50 @@ class ConverseWordDataset(Dataset):
             target = torch.tensor(target, dtype=torch.float)
             # feature, dec_input, target을 반환
             return feature, dec_input, target
+        # y값이 없거나 평가 모드일 때
         else:
             # 디코더의 입력값(SPPPP...)을 tensor로 변환
-            dec_input = torch.tensor(self.y_dec[idx], dtype=torch.int32)
-            # feature, dec_input을 반환, target은 None
-            return feature, dec_input, None
+            dec_input = torch.tensor(self.y_fake[idx], dtype=torch.int32)
+            # 타겟값을 ohe 후 tensor로 변환 -> pred값과 비교하기 위해 ohe
+            # loss 계산을 위해 tensor.float으로 변환
+            target = self.oh_vector[self.y_target[idx]]
+            target = torch.tensor(target, dtype=torch.float)
+            # feature, dec_input을 반환, target은 있을경우 반환(없으면 dec_input과 동일)
+            return feature, dec_input, target
+        
+    def __getitem_ohe(self, idx):
+        # 인코더의 입력값을 ohe
+        feature = self.oh_vector[self.X[idx]]
+        # tensor로 변환
+        feature = torch.tensor(feature, dtype=torch.float)
+        if self.y_target is not None and self.is_train_mode:
+            # 디코더의 입력값과 출력값(SE토근이 추가된 y)을 ohe
+            dec_input = self.oh_vector[self.y_dec[idx]]
+            target = self.oh_vector[self.y_target[idx]]
+            # tensor로 변환
+            dec_input = torch.tensor(dec_input, dtype=torch.float)
+            target = torch.tensor(target, dtype=torch.float)
+            return feature, dec_input, target
+        else:
+            # 디코더의 입력값만 ohe (SPPPP...)
+            dec_input = self.oh_vector[self.y_fake[idx]]
+            target = self.oh_vector[self.y_target[idx]]
+            # tensor로 변환
+            dec_input = torch.tensor(dec_input, dtype=torch.float)
+            target = torch.tensor(target, dtype=torch.float)
+            return feature, dec_input, target
+    
     ######################
     # 데이터셋 내부 함수 정의
     ######################
+    # 모드 변경 함수
+    def train(self):
+        if self.y_target is None:
+            raise Exception('y값이 없습니다.')
+        self.is_train_mode = True
+    def eval(self):
+        self.is_train_mode = False
+    
     # 토큰화
     def __tokenizer(self, datas):
         token_datas = []
@@ -92,6 +141,7 @@ class ConverseWordDataset(Dataset):
             token_datas.append(token_data)
         # 토큰화된 데이터를 numpy 배열로 변환
         return np.array(token_datas)
+    
     # 시작 토큰과 끝 토큰을 추가
     def __add_se_token(self, token_datas, sep_tok='S'):
         add_sep_datas = []
@@ -102,6 +152,7 @@ class ConverseWordDataset(Dataset):
                 add_sep_data = list(token_data) + [self.vocab.char2idx(sep_tok)]
             add_sep_datas.append(add_sep_data)
         return np.array(add_sep_datas)
+    
     # 디코더의 기본 입력값만 생성
     def __get_eval_dec_input(self):
         # 데이터의 총 갯수
@@ -125,7 +176,7 @@ class ConvWordEmbedding(nn.Module):
     
     def forward(self, X):
         return self.embedding(X)
-    pass
+
 # Seq2Seq 레이어
 class ConvWordSeq2Seq(nn.Module):
     def __init__(self, input_size, hidden_size, n_layers=1, bidirectional=False):
@@ -166,7 +217,7 @@ class ConvWordOutput(nn.Module):
         super().__init__()
         self.output_layer = nn.Sequential(
             nn.Linear(input_size, output_size), # 입력값의 차원, 출력값의 차원
-            nn.Softmax(dim=-1),                 # 출력값을 확률로 변환
+            # nn.Softmax(dim=-1),                 # 출력값을 확률로 변환
         )
         
     def forward(self, X):
@@ -176,13 +227,15 @@ class ConvWordOutput(nn.Module):
 # 모델 정의
 ######################
 class ConvWordModel(nn.Module):
-    def __init__(self, vocab_size, embedding_size, hidden_size, n_layers=1, bidirectional=False):
+    def __init__(self, vocab_size, embedding_size, hidden_size, n_layers=1, bidirectional=False, embedding_type='emb'):
         super().__init__()
         num_bi = 2 if bidirectional else 1
+        self.embedding_type = embedding_type
         # 임베딩 레이어
-        self.embedding_layer = ConvWordEmbedding(vocab_size=vocab_size,         # 임베딩할 단어의 갯수
-                                                embedding_size=embedding_size,  # 임베딩 결과의 차원
-                                                )
+        if embedding_type == 'emb':
+            self.embedding_layer = ConvWordEmbedding(vocab_size=vocab_size,         # 임베딩할 단어의 갯수
+                                                    embedding_size=embedding_size,  # 임베딩 결과의 차원
+                                                    )
         # Seq2Seq 레이어
         self.seq2seq_layer = ConvWordSeq2Seq(input_size=embedding_size,     # 인코더, 디코더의 입력값의 차원
                                             hidden_size=hidden_size,        # 히든 레이어의 차원
@@ -195,15 +248,81 @@ class ConvWordModel(nn.Module):
                                             )
     
     def forward(self, X, y):
-        # 인코더의 입력값을 임베딩
-        enc_input = self.embedding_layer(X)
-        # 디코더의 입력값을 임베딩
-        dec_input = self.embedding_layer(y)
+        if self.embedding_type == 'emb':
+            # 인코더의 입력값을 임베딩
+            enc_input = self.embedding_layer(X)
+            # 디코더의 입력값을 임베딩
+            dec_input = self.embedding_layer(y)
+        elif self.embedding_type == 'ohe':
+            enc_input = X
+            dec_input = y
+        # print(enc_input.shape, dec_input.shape)
         # 인코더와 디코더의 입력값을 넣어 dec_output 생성
         dec_output = self.seq2seq_layer(enc_input, dec_input)
         # dec_output을 출력값으로 변환
         output = self.output_layer(dec_output)
         return output
+    
+######################
+# 얼리스탑핑 정의
+######################
+class EarlyStopping():
+    def __init__(self, patience=10, save_path=None, target_score=0, model_name='model'):
+        # 초기화
+        self.best_score = 0
+        self.patience_count = 0
+        self.target_score = target_score
+        self.patience = patience
+        self.save_path = save_path
+        best_model_name = model_name + '_best.pth'
+        self.best_model_path = self.save_path + best_model_name
+        last_model_name = model_name + '_last.pth'
+        self.last_model_path = self.save_path + last_model_name
+    # 얼리 스토핑 여부 확인 함수 정의
+    def is_stop(self, model, score):
+        # 모델 저장(마지막 모델)
+        self.__save_last_model(model)
+        # 베스트 스코어가 타겟 스코어보다 낮을 경우
+        if self.best_score < self.target_score:
+            # 스코어가 이전보다 안좋을 경우
+            if self.best_score > score:
+                # patience 초기화
+                self.patience_count = 0
+                return False
+            # 스코어를 업데이트
+            self.best_score = score
+            # 모델 저장
+            self.__save_best_model(model)
+            # patience 초기화
+            self.patience_count = 0
+            return False
+            
+        
+        # 스코어가 이전보다 좋을 경우
+        if self.best_score < score:
+            # 스코어를 업데이트
+            self.best_score = score
+            # 모델 저장
+            self.__save_best_model(model)
+            # patience 초기화
+            self.patience_count = 0
+            return False
+        
+        # 스코어가 이전보다 좋지 않을 경우 +
+        # 스코어가 타겟 스코어보다 높을 경우
+        # patience 증가
+        self.patience_count += 1
+        # patience가 최대치를 넘을 경우
+        if self.patience_count > self.patience:
+            return True
+        # patience가 최대치를 넘지 않을 경우
+        return False
+    # 모델 저장 함수 정의
+    def __save_best_model(self, model):
+        torch.save(model.state_dict(), self.best_model_path)
+    # 마지막 모델 저장 함수 정의
+    def __save_last_model(self, model):
+        torch.save(model.state_dict(), self.last_model_path)
 
 ######################
 # 트레인 루프 정의
@@ -215,6 +334,8 @@ def train(model, loader, optimizer, device, loss_fn):
     loss_score = None
     # score를 저장할 변수
     acc_score = None
+    # Softmax를 적용하기 위해 모델 생성
+    softmax = nn.Softmax(dim=-1)
     # 학습 루프
     for feature, dec_input, target in loader:
         # device에 데이터 이동
@@ -231,15 +352,18 @@ def train(model, loader, optimizer, device, loss_fn):
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+        # softmax 적용
+        output = softmax(output)
         # loss 기록
         loss_score = loss.item()
         target = target.detach().cpu().numpy().argmax(axis=-1)
         output = output.detach().cpu().numpy().argmax(axis=-1)
-        # print(target.shape, output.shape)
         # score 계산
-        score = accuracy_score(target.argmax(axis=1), output.argmax(axis=1))
+        scores = []
+        for t, o in zip(target, output):
+            scores.append(accuracy_score(t, o))
         # score 기록
-        acc_score = score
+        acc_score = np.mean(scores)
     # loss와 score를 반환
     return loss_score, acc_score
 
@@ -247,18 +371,37 @@ def train(model, loader, optimizer, device, loss_fn):
 # 테스트 루프 정의
 ######################
 @torch.inference_mode()
-def test(model, loader, device):
+def test(model, loader, device, is_target=False):
     # 모델을 평가 모드로 설정
     model.eval()
     # 예측값을 저장할 변수
     pred = None
+    # acc를 저장할 변수
+    acc_score = None
+    # Softmax를 적용하기 위해 모델 저장
+    softmax = nn.Softmax(dim=-1)
     # 평가 루프
-    for feature, dec_input, _ in loader:
+    for feature, dec_input, target in loader:
         # device에 데이터 이동
         feature = feature.to(device)
         dec_input = dec_input.to(device)
         # 모델에 feature와 dec_input을 넣어 예측값 생성
         output = model(feature, dec_input)
-        # 예측값을 pred_list에 추가
+        # softmax 적용
+        output = softmax(output)
+        # 예측값을 pred에 저장
         pred = output.detach().cpu().numpy()
-    return pred
+        # target이 있으면 acc 계산
+        if is_target:
+            # target을 cpu로 이동 후 numpy로 변환
+            target = target.detach().cpu().numpy().argmax(axis=-1)
+            # output을 cpu로 이동 후 numpy로 변환
+            output = output.detach().cpu().numpy().argmax(axis=-1)
+            # acc 계산
+            acc_scores = []
+            for t, o in zip(target, output):
+                acc_scores.append(accuracy_score(t, o))
+            # acc 기록
+            acc_score = np.mean(acc_scores)
+    # 예측값과 acc를 반환
+    return pred, acc_score
